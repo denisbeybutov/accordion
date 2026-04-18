09be58badcad5172790bf98777affbe2a9bca8f9
denisbeybutov <beybutov.denis@mail.ru>
2026-04-18 22:17:44 +0300

    почти готов

--- script.js
+++ script.js
@@ -1,18 +1,63 @@
+//перменные
 const list = document.querySelector('#list');
-const answers = document.querySelectorAll('[data-answer-id]');
+const items = document.querySelectorAll('[data-id]')
 
-list.addEventListener('click', openAndCloseQuestion)
+const questions = [];
 
-function openAndCloseQuestion(event){
-    const qusetionId = event.target.parentElement.dataset.id || event.target.dataset.id;
-    const arrowDown = event.target.parentElement.querySelector('.accordion__down');
-    const arrowUp = event.target.parentElement.querySelector('.accordion__up');
-    answers.forEach(answer => {
-        if(answer.dataset.answerId === qusetionId) {
-            answer.classList.toggle('animation');
-            arrowDown.classList.toggle('hidden');
-            arrowUp.classList.toggle('hidden');
-        }
+//функции
+function initArrOfQuestions(arr){
+    items.forEach(item => {
+        arr.push(
+            {
+                id: item.dataset.id,
+                question: item.querySelector('.accordion__question').textContent,
+                answer: item.querySelector('.accordion__answer').textContent,
+                height: item.querySelector('.accordion__question-main').clientHeight,
+            }
+        )
     })
+    // console.log(arr);
 }
 
+function toggleQuestion(event){
+    
+    const currentQuestionId = event.target.parentElement.dataset.id || 
+                              event.target.parentElement.parentElement.dataset.id;
+
+    
+    items.forEach(item => {
+       const equalId = item.dataset.id === currentQuestionId;
+       const openContains = item.classList.contains('open');
+      if(!openContains && equalId) {
+        open(item, currentQuestionId);
+      } else if (openContains && equalId){
+        close(item,currentQuestionId)
+      }
+    })
+
+}
+
+function open(item, currentQuestionId){
+    const heightOfText = 50 + questions[currentQuestionId-1].height;
+    item.classList.add('open');
+    item.style.setProperty('--myHeight', `${heightOfText}px`);
+    item.style.setProperty('--myHeightPlusFive', `${heightOfText + 5}px`);
+    
+    const down = item.querySelector('.accordion__down');
+    down.classList.add('rotate-up')
+}
+
+function close(item, currentQuestionId){
+    item.classList.remove('open')
+    item.classList.add('close')
+    setTimeout(()=>{item.classList.remove('close')},900)
+    console.log(item.querySelector('.accordion__down'));
+    const down = item.querySelector('.accordion__down');
+    
+}
+
+//программа
+
+initArrOfQuestions(questions);
+list.addEventListener('click', toggleQuestion);
+// list.addEventListener('click', closeQuestion);
